115b68b740c35de10b36da363174972a8ef6768a
Web-krush <161991436+Web-krush@users.noreply.github.com>
2024-03-03 12:27:24 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className='text-xl font-extrabold bg-slate-950 p-3  text-[#fff]  text-center select-none'>
-      <p className='text-sm'>&copy; 2024 MADE ❤️‍🔥 SPIDY</p>
+      <p className='text-sm'>&copy; 2024 </p>
     </div>
   );
 };
